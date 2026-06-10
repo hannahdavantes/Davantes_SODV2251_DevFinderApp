f68b1b21054677f8ba7cbf6e3677d1cf4e3d2546
Hannah Davantes <hannahdavantes@yahoo.com>
2026-06-10 14:38:11 -0600

    Added the MapScreen with temporary markers

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,11 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
-type Props = {};
-const MapScreen = (props: Props) => {
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MapView, { Marker } from "react-native-maps";
+import { mockUsers } from "../data/mockUsers";
+import { theme } from "../constants/theme";
+
+export default function MapScreen() {
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 52.269,
+          longitude: -113.8116,
+          latitudeDelta: 8,
+          longitudeDelta: 8,
+        }}
+      >
+        {mockUsers.map((user) => (
+          <Marker
+            key={user.username}
+            coordinate={{
+              latitude: user.latitude,
+              longitude: user.longitude,
+            }}
+            title={user.name}
+            description={user.username}
+          />
+        ))}
+      </MapView>
+    </SafeAreaView>
   );
-};
-export default MapScreen;
-const styles = StyleSheet.create({});
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.primaryDark,
+  },
+  map: {
+    flex: 1,
+  },
+});
