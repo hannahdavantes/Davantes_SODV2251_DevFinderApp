f0deefeb9e67d84adc82054923f658d32a3f41ca
Hannah Davantes <hannahdavantes@yahoo.com>
2026-06-10 12:13:20 -0600

    Add mock user data

---
+++ src/data/mockUsers.ts
@@ -0,0 +1,65 @@
+import { MockUser } from "../types/User";
+
+//Hardcoded community members displayed on the map
+export const mockUsers: MockUser[] = [
+  {
+    username: "octocat",
+    name: "Octo Cat",
+    latitude: 51.0447,
+    longitude: -114.0719,
+  },
+  {
+    username: "coder_jane",
+    name: "Jane Doe",
+    latitude: 51.0486,
+    longitude: -114.0708,
+  },
+  {
+    username: "dev_dan",
+    name: "Dan Smith",
+    latitude: 51.0501,
+    longitude: -114.0853,
+  },
+  {
+    username: "pixel_pete",
+    name: "Peter Parker",
+    latitude: 51.0394,
+    longitude: -114.0532,
+  },
+  {
+    username: "frontend_fany",
+    name: "Fany G.",
+    latitude: 51.0412,
+    longitude: -114.1324,
+  },
+  {
+    username: "backend_bob",
+    name: "Bob Builder",
+    latitude: 51.062,
+    longitude: -114.021,
+  },
+  {
+    username: "git_gordon",
+    name: "Gordon Freeman",
+    latitude: 52.269,
+    longitude: -113.8115,
+  },
+  {
+    username: "ruby_rose",
+    name: "Rose Miller",
+    latitude: 53.5461,
+    longitude: -113.4938,
+  },
+  {
+    username: "python_paul",
+    name: "Paul Atreides",
+    latitude: 53.5225,
+    longitude: -113.5256,
+  },
+  {
+    username: "script_sara",
+    name: "Sara Connor",
+    latitude: 53.6316,
+    longitude: -113.3239,
+  },
+];
