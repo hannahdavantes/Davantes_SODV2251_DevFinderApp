93711ac0e7eefbdb69e22d503870d45d3a63b61c
Hannah Davantes <hannahdavantes@yahoo.com>
2026-06-12 12:07:14 -0600

    Fetch github username using route params

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,9 +1,48 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useRoute, RouteProp } from "@react-navigation/native";
+import { type GitHubUser, fetchGitHubUser } from "../services/gitHubService";
+import { RootStackParamList } from "../navigation/types";
+import { useEffect, useState } from "react";
+
 type Props = {};
 const ProfileScreen = (props: Props) => {
+  const route = useRoute<RouteProp<RootStackParamList, "Profile">>();
+  const { username } = route.params;
+
+  const [user, setUser] = useState<GitHubUser | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    //Reset error/loading state
+    setError("");
+    setIsLoading(true);
+
+    const checkUser = async () => {
+      try {
+        //Check if GitHub user exist
+        const gitHubUser: GitHubUser = await fetchGitHubUser(username);
+        setUser(gitHubUser);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError("Something went wrong. Please try again.");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkUser();
+  }, []);
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      <View>
+        {isLoading && <Text>Loading...</Text>}
+        {error && <Text>{error}</Text>}
+        {user && <Text>{user.login}</Text>}
+      </View>
     </View>
   );
 };
